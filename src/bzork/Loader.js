638ce5346574ca824f8798a55fa5f70c5669cfa3
Kyle Hargraves <pd@krh.me>
2011-02-14 16:26:09 -0600

    pass sloppily
    jDataView behaves like a stream, and i will soon need to pay more attention
    to that fact. but for now, hey look, i'm green.

--- src/bzork/Loader.js
+++ src/bzork/Loader.js
@@ -1,10 +1,29 @@
 bzork.Loader = function(data) {
   this.blob = data;
   this.dataView = new jDataView(this.blob);
+  this.header = new bzork.Loader.Header(new jDataView(this.blob, 0, 64));
 };
 
 bzork.Loader.prototype.loadStory = function() {
+  this.header.load();
   var story = new bzork.Story();
-  story.zcodeVersion = this.dataView.getUint8();
+  story.zcodeVersion = this.header.zcodeVersion;
+  story.releaseNumber = this.header.releaseNumber;
+  story.serial = this.header.serial;
   return story;
+};
+
+bzork.Loader.Header = function(dataView) {
+  this.dataView = dataView;
+};
+
+bzork.Loader.Header.prototype.load = function() {
+  this.zcodeVersion = this.dataView.getUint8(0);
+  this.releaseNumber = this.dataView.getUint16(2);
+
+  this.serial = '';
+  for (var i = 0; i < 6; i++) {
+    var c = this.dataView.getUint8(16 + i);
+    this.serial += String.fromCharCode(c);
+  };
 };
