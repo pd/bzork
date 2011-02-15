f703d17593b6d656d168280597f4f769290a9eed
Kyle Hargraves <pd@krh.me>
2011-02-14 22:16:00 -0600

    implement the rest of header (sans flags)

--- public/javascripts/bzork/Memory.js
+++ public/javascripts/bzork/Memory.js
@@ -17,7 +17,15 @@ bzork.Memory.Header = function(buffer) {
 };
 
 bzork.Memory.Header.prototype.getZcodeVersion = function() {
-  return this._memory.getUint8(0);
+  return this._memory.getUint8(0x0);
+};
+
+bzork.Memory.Header.prototype.getReleaseNumber = function() {
+  return this._memory.getUint16(0x2);
+};
+
+bzork.Memory.Header.prototype.getHighMemAddr = function() {
+  return this._memory.getUint16(0x4);
 };
 
 bzork.Memory.Header.prototype.getStartPC = function() {
@@ -34,6 +42,37 @@ bzork.Memory.Header.prototype.getObjectTableAddr = function() {
 
 bzork.Memory.Header.prototype.getGlobalTableAddr = function() {
   return this._memory.getUint16(0xc);
+};
+
+bzork.Memory.Header.prototype.getStaticMemAddr = function() {
+  return this._memory.getUint16(0xe);
+};
+
+bzork.Memory.Header.prototype.getAbbrevTableAddr = function() {
+  return this._memory.getUint16(0x18);
+};
+
+bzork.Memory.Header.prototype.getFileSize = function() {
+  var size = this._memory.getUint16(0x1a),
+      zver = this.getZcodeVersion();
+
+  if (zver <= 3)
+    return size * 2;
+  else if (zver <= 5)
+    return size * 4;
+  else
+    return size * 8;
+};
+
+bzork.Memory.Header.prototype.getChecksum = function() {
+  return this._memory.getUint16(0x1c);
+};
+
+bzork.Memory.Header.prototype.getSerial = function() {
+  var serial = [];
+  for (var i = 0; i < 6; i++)
+    serial.push( String.fromCharCode(this._memory.getUint8(0x12 + i)) );
+  return serial.join('');
 };
 
 bzork.Memory.Dictionary = function(buffer) {
--- spec/javascripts/bzork/MemorySpec.js
+++ spec/javascripts/bzork/MemorySpec.js
@@ -11,6 +11,14 @@ describe("bzork.Memory.Header", function() {
       expect(this.header.getZcodeVersion()).toEqual(3);
     });
 
+    it("should know the release number", function() {
+      expect(this.header.getReleaseNumber()).toEqual(88);
+    });
+
+    it("should know the high memory base address", function() {
+      expect(this.header.getHighMemAddr()).toEqual(0x4e37);
+    });
+
     it("should know the first instruction's address", function() {
       expect(this.header.getStartPC()).toEqual(0x4f05);
     });
@@ -25,6 +33,26 @@ describe("bzork.Memory.Header", function() {
 
     it("should know the global variable address", function() {
       expect(this.header.getGlobalTableAddr()).toEqual(0x2271);
+    });
+
+    it("should know the static memory base address", function() {
+      expect(this.header.getStaticMemAddr()).toEqual(0x2e53);
+    });
+
+    it("should know the abbreviations tables address", function() {
+      expect(this.header.getAbbrevTableAddr()).toEqual(0x01f0);
+    });
+
+    it("should know the file size", function() {
+      expect(this.header.getFileSize()).toEqual(0x14b8c);
+    });
+
+    it("should know the checksum", function() {
+      expect(this.header.getChecksum()).toEqual(0xa129);
+    });
+
+    it("should know the story's serial", function() {
+      expect(this.header.getSerial()).toEqual("840726");
     });
   });
 });
