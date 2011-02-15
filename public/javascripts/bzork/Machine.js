df74579d5813709ca4e8a8d4913a01ccf0b42f88
Kyle Hargraves <pd@krh.me>
2011-02-15 00:16:48 -0600

    Machine sets the alphabet while booting

--- public/javascripts/bzork/Machine.js
+++ public/javascripts/bzork/Machine.js
@@ -2,6 +2,8 @@ bzork.Machine = function(buffer) {
   this.memory = new bzork.Memory(buffer);
   this.pc = this.memory.header.getStartPC();
   this.stack = new bzork.Stack();
+
+  bzork.Zscii.setAlphabet(this.memory.header.getZcodeVersion());
 };
 
 bzork.Stack = function() {
--- spec/javascripts/bzork/MachineSpec.js
+++ spec/javascripts/bzork/MachineSpec.js
@@ -1,2 +1,7 @@
 describe("bzork.Machine", function() {
+  it("should set the current alphabet based on the Z-code version number", function() {
+    spyOn(bzork.Zscii, 'setAlphabet');
+    new bzork.Machine(bzork.spec.storyData['zork1']);
+    expect(bzork.Zscii.setAlphabet).toHaveBeenCalledWith(3);
+  });
 });
