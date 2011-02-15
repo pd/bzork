e479d4d155ff8b23885c8f54fabcf7419113274a
Kyle Hargraves <pd@krh.me>
2011-02-14 19:03:07 -0600

    stay within the DataView API
    jDataView provides seek() and the byteOffset values as optional,
    but it's not particularly useful (at least not yet), so stick with
    what will be drop-in replaceable in the future

--- public/javascripts/bzork/Loader.js
+++ public/javascripts/bzork/Loader.js
@@ -11,18 +11,18 @@ bzork.Loader.prototype.loadStory = function() {
 
 bzork.Loader.prototype.loadHeader = function(story) {
   var dv = this.dataView;
-  dv.seek(0);
 
-  story.header.zcodeVersion = dv.getUint8();
-  dv.seek(2); // skip flags for now
-  story.header.releaseNumber = dv.getUint16();
-  story.header.highMemAddr = dv.getUint16();
-  story.header.startPC = dv.getUint16();
-  story.header.dictionaryAddr = dv.getUint16();
-  story.header.objectTableAddr = dv.getUint16();
-  story.header.globalTableAddr = dv.getUint16();
+  story.header.zcodeVersion = dv.getUint8(0);
+  story.header.releaseNumber = dv.getUint16(2);
+  story.header.highMemAddr = dv.getUint16(4);
+  story.header.startPC = dv.getUint16(6);
+  story.header.dictionaryAddr = dv.getUint16(8);
+  story.header.objectTableAddr = dv.getUint16(10);
+  story.header.globalTableAddr = dv.getUint16(12);
+  story.header.staticMemAddr = dv.getUint16(14);
+  story.header.abbrevTableAddr = dv.getUint16(24);
 
   story.header.serial = '';
   for (var i = 0; i < 6; i++)
-    story.header.serial += String.fromCharCode( dv.getUint8(16 + i) );
+    story.header.serial += String.fromCharCode( dv.getUint8(18 + i) );
 };
