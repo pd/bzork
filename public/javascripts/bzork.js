a4d1f6f6cf40c42dc39e84f588428e884624a338
Kyle Hargraves <pd@krh.me>
2011-02-16 12:26:51 -0600

    create bzork.vm namespace, stub bzork.vm.Instruction

--- public/javascripts/bzork.js
+++ public/javascripts/bzork.js
@@ -1,5 +1,7 @@
 var bzork = (function() {
+  // Set up namespaces and suchlike
   return {
+    vm: {},
     version: "0.1"
   };
-});
+})();
---
+++ public/javascripts/bzork/vm/Instruction.js
@@ -0,0 +1,4 @@
+bzork.vm.Instruction = function(machine, addr) {
+  this._machine = machine;
+  this._addr = addr;
+};
