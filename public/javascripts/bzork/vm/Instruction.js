531f49d57cc0b25ea9758d7abdbfa0890f24bd46
Kyle Hargraves <pd@krh.me>
2011-02-16 13:37:34 -0600

    able to read operands for the start PC of zork1, time to gut&spec

--- public/javascripts/bzork/vm/Instruction.js
+++ public/javascripts/bzork/vm/Instruction.js
@@ -17,6 +17,13 @@ bzork.vm.Instruction.OpCounts = {
   VAR: 3
 };
 
+bzork.vm.Instruction.OpTypes = {
+  LARGE: 0,
+  SMALL: 1,
+  VAR:   2,
+  OMIT:  3
+};
+
 bzork.vm.Instruction.prototype.getOpcodeByte = function() {
   return this._machine.getUint8(this._addr);
 };
@@ -70,4 +77,95 @@ bzork.vm.Instruction.prototype.getOperandCount = function() {
   default:
     throw "Impossible!";
   }
+};
+
+bzork.vm.Instruction.prototype.getOperandTypes = function() {
+  var opbyte = this.getOpcodeByte();
+
+  switch (this.getForm()) {
+  case bzork.vm.Instruction.Forms.SHORT:
+    return this._map2bitOperandType(opbyte & 0x30);
+  case bzork.vm.Instruction.Forms.LONG:
+    return [this._map1bitOperandType(opbyte & 0x40),
+            this._map1bitOperandType(opbyte & 0x20)];
+  case bzork.vm.Instruction.Forms.VAR:
+    var bitfield = this._machine.getUint8(this._addr + 1),
+        types = [];
+    for (var n = 6; n >= 0; n -= 2) {
+      var bits = (bitfield >> n) & 0x3;
+      types.push(this._map2bitOperandType(bits));
+    }
+    return types;
+  }
+};
+
+bzork.vm.Instruction.prototype.getOperands = function() {
+  var offset = 0,
+      form = this.getForm(),
+      optypes = this.getOperandTypes(),
+      operands = [];
+
+  if (form === bzork.vm.Instruction.Forms.VAR ||
+      form === bzork.vm.Instruction.Forms.EXT)
+    offset += 1; // could be +=2 for "double variable" VARs, see 4.4.3.1
+
+  switch (this.getOperandCount()) {
+  case bzork.vm.Instruction.OpCounts.OP0:
+    return [];
+  case bzork.vm.Instruction.OpCounts.OP1:
+    return [this._getOperand(offset, optypes[0])];
+  case bzork.vm.Instruction.OpCounts.OP2:
+  case bzork.vm.Instruction.OpCounts.VAR:
+    for (var i = 0; i < optypes.length; i++) {
+      operands.push( this._getOperand(offset, optypes[i]) );
+      offset += this._operandSize(optypes[i]);
+    }
+    return operands;
+  }
+};
+
+bzork.vm.Instruction.prototype._getOperand = function(offset, type) {
+  if (type === bzork.vm.Instruction.OpTypes.OMIT)
+    return undefined;
+
+  var form = this.getForm();
+
+  // offset is from the beginning of the operand values,
+  // but we need the full offset into memory
+  offset += this._addr; // beginning of instruction
+  if (form === bzork.vm.Instruction.Forms.VAR ||
+      form === bzork.vm.Instruction.Forms.EXT)
+    offset += 1; // operand types bit field
+
+  if (type === bzork.vm.Instruction.OpTypes.LARGE)
+    return this._machine.getUint16(offset);
+  else
+    return this._machine.getUint8(offset);
+};
+
+bzork.vm.Instruction.prototype._operandSize = function(type) {
+  if (type === bzork.vm.Instruction.OpTypes.LARGE)
+    return 2;
+  else if (type === bzork.vm.Instruction.OpTypes.OMIT)
+    return 0;
+  else
+    return 1;
+};
+
+bzork.vm.Instruction.prototype._map2bitOperandType = function(bits) {
+  switch (bits) {
+  case 0: return bzork.vm.Instruction.OpTypes.LARGE;
+  case 1: return bzork.vm.Instruction.OpTypes.SHORT;
+  case 2: return bzork.vm.Instruction.OpTypes.VAR;
+  case 3: return bzork.vm.Instruction.OpTypes.OMIT;
+  default: throw "Invalid operand type value " + bits;
+  }
+};
+
+bzork.vm.Instruction.prototype._map1bitOperandType = function(bit) {
+  bit = bit & 0x1;
+  if (bit === 1)
+    return bzork.vm.Instruction.OpTypes.VAR;
+  else if (bit === 0)
+    return bzork.vm.Instruction.OpTypes.SHORT;
 };
