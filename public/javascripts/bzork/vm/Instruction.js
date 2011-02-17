64dc0d0101c936dcc8085a1752281b3126a4ff6b
Kyle Hargraves <pd@krh.me>
2011-02-17 10:32:54 -0600

    move Opcodes into Instruction proper; no need for InstructionInfo object type

--- public/javascripts/bzork/vm/Instruction.js
+++ public/javascripts/bzork/vm/Instruction.js
@@ -4,27 +4,6 @@ bzork.vm.Instruction = function(machine, addr) {
   this._stealImplMethods();
 };
 
-bzork.vm.Instruction.Forms = {
-  LONG:  'LONG',
-  SHORT: 'SHORT',
-  EXT:   'EXT',
-  VAR:   'VAR'
-};
-
-bzork.vm.Instruction.OpCounts = {
-  OP0: 0,
-  OP1: 1,
-  OP2: 2,
-  VAR: 3
-};
-
-bzork.vm.Instruction.OpTypes = {
-  LARGE: 0,
-  SMALL: 1,
-  VAR:   2,
-  OMIT:  3
-};
-
 bzork.vm.Instruction.prototype.uniqueKey = function() {
   var operandCount = this._is8OP && this._is8OP() ? bzork.vm.Instruction.OpCounts.VAR :
     this.getOperandCount();
@@ -140,7 +119,7 @@ bzork.vm.Instruction.prototype._stealImplMethods = function() {
 };
 
 bzork.vm.Instruction.prototype._getInstructionInfo = function() {
-  return bzork.vm.InstructionInfo.DB[this.uniqueKey()];
+  return bzork.vm.InstructionDB[this.uniqueKey()];
 };
 
 bzork.vm.Instruction.prototype._getOperand = function(offset, type) {
@@ -191,4 +170,128 @@ bzork.vm.Instruction.prototype._getBranchOffsetSize = function() {
 bzork.vm.Instruction.prototype._getStringAddr = function() {
   // neither print nor print_ret store or branch, so this is always safe
   return this._getAfterOperandsAddr();
+};
+
+///// Constants
+
+bzork.vm.Instruction.Forms = {
+  LONG:  'LONG',
+  SHORT: 'SHORT',
+  EXT:   'EXT',
+  VAR:   'VAR'
+};
+
+bzork.vm.Instruction.OpCounts = {
+  OP0: 0,
+  OP1: 1,
+  OP2: 2,
+  VAR: 3
+};
+
+bzork.vm.Instruction.OpTypes = {
+  LARGE: 0,
+  SMALL: 1,
+  VAR:   2,
+  OMIT:  3
+};
+
+bzork.vm.Instruction.Opcodes = {
+  // 0OP
+  rtrue: 0,
+  rfalse: 1,
+  print: 2,
+  print_ret: 3,
+  nop: 4,
+  save: 5,
+  restore: 6,
+  restart: 7,
+  ret_popped: 8,
+  pop: 9,
+  quit: 10,
+  new_line: 11,
+  show_status: 12,
+  verify: 13,
+  pirate: 15,
+
+  // 1OP
+  jz: 0,
+  get_sibling: 1,
+  get_child: 2,
+  get_parent: 3,
+  get_prop_len: 4,
+  inc: 5,
+  dec: 6,
+  print_addr: 7,
+  call_1s: 8,
+  remove_obj: 9,
+  print_obj: 10,
+  ret: 11,
+  jump: 12,
+  print_paddr: 13,
+  load: 14,
+  not: 15,
+
+  // 2OP
+  je: 1,
+  jl: 2,
+  jg: 3,
+  dec_chk: 4,
+  inc_chk: 5,
+  jin: 6,
+  test: 7,
+  or: 8,
+  and: 9,
+  test_attr: 10,
+  set_attr: 11,
+  clear_attr: 12,
+  store: 13,
+  insert_obj: 14,
+  loadw: 15,
+  loadb: 16,
+  get_prop: 17,
+  get_prop_addr: 18,
+  get_next_prop: 19,
+  add: 20,
+  sub: 21,
+  mul: 22,
+  div: 23,
+  mod: 24,
+  call_2s: 25,
+  call_2n: 26,
+  set_colour: 27,
+  'throw': 28,
+
+  // VAROP
+  call: 0,
+  storew: 1,
+  storeb: 2,
+  put_prop: 3,
+  sread: 4,
+  print_char: 5,
+  print_num: 6,
+  random: 7,
+  push: 8,
+  pull: 9,
+  split_window: 10,
+  set_window: 11,
+  call_vs2: 12,
+  erase_window: 13,
+  erase_line: 14,
+  set_cursor: 15,
+  get_cursor: 16,
+  set_text_style: 17,
+  buffer_mode: 18,
+  output_stream: 19,
+  input_stream: 20,
+  sound_effect: 21,
+  read_char: 22,
+  scan_table: 23,
+  not: 24,
+  call_vn: 25,
+  call_vn2: 26,
+  tokenise: 27,
+  encode_text: 28,
+  copy_table: 29,
+  print_table: 30,
+  check_arg_count: 31
 };
--- public/javascripts/bzork/vm/InstructionInfo.js
+++ public/javascripts/bzork/vm/InstructionInfo.js
@@ -1,131 +1,20 @@
-bzork.vm.InstructionInfo = function(name, stores, branches, stringed, version) {
-  if (!version)
-    version = 1;
+bzork.vm.InstructionDB = {};
 
-  this.name     = name;
-  this.stores   = stores;
-  this.branches = branches;
-  this.stringed = stringed;
-  this.version  = version;
-};
-
-bzork.vm.InstructionInfo.Opcodes = {
-  // 0OP
-  rtrue: 0,
-  rfalse: 1,
-  print: 2,
-  print_ret: 3,
-  nop: 4,
-  save: 5,
-  restore: 6,
-  restart: 7,
-  ret_popped: 8,
-  pop: 9,
-  quit: 10,
-  new_line: 11,
-  show_status: 12,
-  verify: 13,
-  pirate: 15,
-
-  // 1OP
-  jz: 0,
-  get_sibling: 1,
-  get_child: 2,
-  get_parent: 3,
-  get_prop_len: 4,
-  inc: 5,
-  dec: 6,
-  print_addr: 7,
-  call_1s: 8,
-  remove_obj: 9,
-  print_obj: 10,
-  ret: 11,
-  jump: 12,
-  print_paddr: 13,
-  load: 14,
-  not: 15,
-
-  // 2OP
-  je: 1,
-  jl: 2,
-  jg: 3,
-  dec_chk: 4,
-  inc_chk: 5,
-  jin: 6,
-  test: 7,
-  or: 8,
-  and: 9,
-  test_attr: 10,
-  set_attr: 11,
-  clear_attr: 12,
-  store: 13,
-  insert_obj: 14,
-  loadw: 15,
-  loadb: 16,
-  get_prop: 17,
-  get_prop_addr: 18,
-  get_next_prop: 19,
-  add: 20,
-  sub: 21,
-  mul: 22,
-  div: 23,
-  mod: 24,
-  call_2s: 25,
-  call_2n: 26,
-  set_colour: 27,
-  'throw': 28,
-
-  // VAROP
-  call: 0,
-  storew: 1,
-  storeb: 2,
-  put_prop: 3,
-  sread: 4,
-  print_char: 5,
-  print_num: 6,
-  random: 7,
-  push: 8,
-  pull: 9,
-  split_window: 10,
-  set_window: 11,
-  call_vs2: 12,
-  erase_window: 13,
-  erase_line: 14,
-  set_cursor: 15,
-  get_cursor: 16,
-  set_text_style: 17,
-  buffer_mode: 18,
-  output_stream: 19,
-  input_stream: 20,
-  sound_effect: 21,
-  read_char: 22,
-  scan_table: 23,
-  not: 24,
-  call_vn: 25,
-  call_vn2: 26,
-  tokenise: 27,
-  encode_text: 28,
-  copy_table: 29,
-  print_table: 30,
-  check_arg_count: 31
-};
-
-bzork.vm.InstructionInfo.DB = {};
-
-// Load all of the opcodes in the standard into the DB
-// Gah. Once I've got this functioning I'll probably just
-// dump it as JSON and plop that into this file instead.
 (function() {
 
   function addEntry(name, opcount, stores, branches, stringed, version) {
-    var opcode = bzork.vm.InstructionInfo.Opcodes[name];
+    var opcode = bzork.vm.Instruction.Opcodes[name];
     var key  = opcount + ":" + opcode;
-    var info = new bzork.vm.InstructionInfo(name, stores, branches, stringed, version);
+    var info = {
+      name: name, opcount: opcount, opcode: opcode,
+      stores: stores, branches: branches, stringed: stringed,
+      version: (version || 1)
+    };
 
-    if (bzork.vm.InstructionInfo.DB[key])
+    if (bzork.vm.InstructionDB[key])
       throw "Adding duplicate entry to instruction DB: " + key;
 
-    bzork.vm.InstructionInfo.DB[key] = info;
+    bzork.vm.InstructionDB[key] = info;
   }
 
   function addBasic(name, opcount, version) {
