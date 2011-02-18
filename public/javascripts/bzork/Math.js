3448f1ad9130475ae5ac6c21fb89ee53df2477d9
Kyle Hargraves <pd@krh.me>
2011-02-17 21:37:11 -0600

    Math.toUint16, toInt16; specs + expose a bug in toInt14

--- public/javascripts/bzork/Math.js
+++ public/javascripts/bzork/Math.js
@@ -3,9 +3,24 @@
 // casts throughout the code.
 bzork.Math = (function() {
   return {
+    toUint16: function(word) {
+      if (word >= 0)
+        return word;
+      else
+        return 0xffff + (word + 1);
+    },
+
+    toInt16: function(word) {
+      if (word > 0x7fff)
+        return -(0xffff - (word - 1));
+      else
+        return word;
+    },
+
     toInt14: function(word) {
+      word = word & 0x3fff;
       if (word > 0x1fff) // max signed (=13bit) value
-        return -(0x3fff - word - 1); // max unsigned (=14bit) value
+        return -(0x3fff - (word - 1)); // max unsigned (=14bit) value
       else
         return word;
     }
