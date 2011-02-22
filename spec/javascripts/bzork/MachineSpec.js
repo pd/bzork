e226b87409c54a546b3a42ff6fea252c5145e4b6
Kyle Hargraves <pd@krh.me>
2011-02-22 00:26:10 -0600

    add what is essentially an integration spec for zork1

--- spec/javascripts/bzork/MachineSpec.js
+++ spec/javascripts/bzork/MachineSpec.js
@@ -1,4 +1,9 @@
 describe("bzork.Machine", function() {
-  // ... hrm
+  it("should do as much as is currently expected of it", function() {
+    var machine = new bzork.Machine(bzork.spec.storyData['zork1']);
+    expect(function() {
+      machine.run()
+    }).toThrow("Unimplemented instruction: jz");
+  });
 });
 
