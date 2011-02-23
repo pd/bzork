8cc709eb54484f2176d3d7552b42d1d6bc06a62e
Kyle Hargraves <pd@krh.me>
2011-02-22 20:55:34 -0600

    back to focusing on zork1

--- spec/javascripts/bzork/MachineSpec.js
+++ spec/javascripts/bzork/MachineSpec.js
@@ -2,14 +2,14 @@ describe("bzork.Machine", function() {
   it("should run zork1 as far as currently expected", function() {
     var machine = new bzork.Machine(bzork.spec.storyData['zork1']);
     expect(function() {
-      machine.run()
+      machine.run('debug-mode')
     }).toThrow("Unimplemented instruction: put_prop");
   });
 
   it("should run ZTUU as far as currently expected", function() {
     var machine = new bzork.Machine(bzork.spec.storyData['ztuu']);
     expect(function() {
-      machine.run('debug-mode')
+      machine.run()
     }).not.toThrow();
   });
 });
